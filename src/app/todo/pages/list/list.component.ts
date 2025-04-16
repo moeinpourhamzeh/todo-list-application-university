5ca227db4c77c1383fbb7cc80700508362bfc5d3
Moein <64212565+moeinpourhamzeh@users.noreply.github.com>
2025-04-16 17:42:07 +0330

    ADD: Delete function added to the component

--- src/app/todo/pages/list/list.component.ts
+++ src/app/todo/pages/list/list.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NoteModel } from '../../../_models/note.model';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  notesList: NoteModel[] = [
+    new NoteModel(1, 'Test Note', 'Test Description Note', new Date()),
+    new NoteModel(2, 'Test Note2', 'Test Description Note', new Date()),
+    new NoteModel(3, 'Test Note3', 'Test Description Note', new Date()),
+  ]
 
+  deleteNote(id: number) {
+    var index = this.notesList.findIndex(x => x.id === id)
+    this.notesList.splice(index, 1);
+  }
 }
